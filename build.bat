493a6d2bee12f5b34cd808775c4e8f4d025893ec
blindelectron <jump.leper@gmail.com>
2022-10-03 17:57:33 -0500

    fixes build script to make the np.cf file

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@ python -m nuitka --standalone estarter.py
 md estarter.dist\s
 copy  s estarter.dist\s
 copy config_default.ini estarter.dist\config_default.ini
-copy nul estarter.dist/np.cf
+type nul>> estarter.dist\np.cf
 pause
