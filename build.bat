c1204811a213138626731b95658d2988ec017cee
blindelectron <jump.leper@gmail.com>
2022-10-10 11:56:53 -0500

    fixes .gitignore so that the lib folder from sound_lib is not excluded.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-python -m nuitka --standalone estarter.py
+python -m nuitka --standalone --include-package=estarter  estarter.py
 md estarter.dist\s
 copy  s estarter.dist\s
 copy config_default.ini estarter.dist\config_default.ini
